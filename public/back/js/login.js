3a25a06b54afbae7f5d6fbb93e83108b10fd9c5a
WenYuanZhi <544268402@qq.com>
2019-08-27 11:52:06 +0800

    配置表单校验图标

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,14 +1,22 @@
 $(function () {
+    
+    // 使用bootstrapValidator插件进行表单验证
     $('#form').bootstrapValidator({
+        // 配置校验图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',    //检验成功
+            invalid: 'glyphicon glyphicon-remove',  //检验失败
+            validating: 'glyphicon glyphicon-refresh'
+        },
         fields: {
             username: {
                 validators: {
                     notEmpty: {
                         message: "用户名不能为空"
                     },
-                    stringLength:{
-                        min:2,
-                        max:6,
+                    stringLength: {
+                        min: 2,
+                        max: 6,
                         message: "用户名长度必须是2-6位"
                     }
                 }
@@ -18,9 +26,9 @@ $(function () {
                     notEmpty: {
                         message: "密码不能为空"
                     },
-                    stringLength:{
-                        min:6,
-                        max:12,
+                    stringLength: {
+                        min: 6,
+                        max: 12,
                         message: "密码长度必须是6-12位"
                     }
                 }
