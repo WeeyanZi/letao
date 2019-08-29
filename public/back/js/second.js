48870631e396e790aa8a5709d867ff7403e2b212
WenYuanZhi <544268402@qq.com>
2019-08-29 10:24:59 +0800

    二级分类完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -46,6 +46,10 @@ $(function () {
     $('.dropdown-menu').on('click', 'a', function () {
         var txt = $(this).text();
         $('#dropdownText').text(txt);
+
+        var id = $(this).data('id');
+        $('[name="categoryId"]').val(id);
+        $("#form").data('bootstrapValidator').updateStatus('categoryId', 'VALID');
     })
 
     $("#fileupload").fileupload({
@@ -53,7 +57,74 @@ $(function () {
         //e：事件对象
         //data：图片上传后的对象，通过data.result.picAddr可以获取上传后的图片地址
         done: function (e, data) {
-            $('#imgBox img').attr('src', data.result.picAddr);
+            var imgUrl = data.result.picAddr;
+            $('#imgBox img').attr('src', imgUrl);
+            $('[name="brandLogo"]').val(imgUrl);
+            $("#form").data('bootstrapValidator').updateStatus('brandLogo', 'VALID');
         }
     });
+
+    //使用表单校验插件
+    $('#form').bootstrapValidator({
+        //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)']
+        excluded: [],
+
+        //2. 指定校验时的图标显示，默认是bootstrap风格
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+
+        //3. 指定校验字段
+        fields: {
+            //校验用户名，对应name表单的name属性
+            categoryId: {
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '请选择一级分类'
+                    }
+                }
+            },
+            brandName: {
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '请选择二级分类'
+                    }
+                }
+            },
+            brandLogo: {
+                validators: {
+                    //不能为空
+                    notEmpty: {
+                        message: '请上传图片'
+                    }
+                }
+            },
+        }
+    });
+
+    $("#form").on('success.form.bv', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: '/category/addSecondCategory',
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function (info) {
+                if (info.success) {
+                    $('#addModal').modal('hide');
+                    currentPage = 1;
+                    render();
+                    $("#form").data('bootstrapValidator').resetForm(true);
+                    $('#dropdownText').text('请选择一级分类');
+                    $('#imgBox img').attr('src', './images/none.png');
+                }
+            }
+        })
+    });
+
+
 })
