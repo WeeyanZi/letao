6e85efa7ac9046779ba8422ab0f843b53f0fc776
WenYuanZhi <544268402@qq.com>
2019-08-31 10:58:03 +0800

    搜索列表全部完成

--- public/front/js/searchList.js
+++ public/front/js/searchList.js
@@ -2,16 +2,19 @@ $(function () {
     var key = getSearch('key');
     $('.search_input').val(key);
 
-    render();
+    var currentPage = 1;
+    var pageSize = 2;
 
-    function render() {
+    // render();
+
+    function render(callback) {
         // <div class="loading"></div>
-        $('.lt_product').html('<div class="loading"></div>');
+        // $('.lt_product').html('<div class="loading"></div>');
 
         var params = {};
         params.proName = $('.search_input').val();
-        params.page = 1;
-        params.size = 100;
+        params.page = currentPage;
+        params.pageSize = pageSize;
 
         var $current = $('.lt_sort a.current');
         if ($current.length > 0) {
@@ -28,8 +31,7 @@ $(function () {
                 data: params,
                 dataType: 'json',
                 success: function (info) {
-                    var htmlStr = template('productTpl', info);
-                    $('.lt_product').html(htmlStr)
+                    callback && callback(info);
                 }
             })
         }, 500);
@@ -42,7 +44,8 @@ $(function () {
             mui.toast('请输入搜索关键字', { duration: 2000 });
             return;
         }
-        render();
+
+        mui('.mui-scroll-wrapper').pullRefresh().pulldownLoading();
 
         var history = localStorage.getItem('search_list') || '[]';
         var arr = JSON.parse(history);
@@ -60,13 +63,54 @@ $(function () {
 
     })
 
-    $('.lt_sort a[data-type]').click(function () {
+    $('.lt_sort a[data-type]').on('tap', (function () {
+        console.log(123456);
         if ($(this).hasClass('current')) {
             $(this).find('i').toggleClass("fa-angle-up").toggleClass("fa-angle-down");
         } else {
             $(this).addClass('current').siblings().removeClass('current');
         }
-        render();
+        mui('.mui-scroll-wrapper').pullRefresh().pulldownLoading();
+    }))
+
+
+    // 下拉刷新  上拉记载
+    mui.init({
+        pullRefresh: {
+            container: ".mui-scroll-wrapper",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+            down: {
+                auto: true,
+                callback: function () {    //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+                    currentPage = 1;
+                    render(function (info) {
+                        var htmlStr = template('productTpl', info);
+                        $('.lt_product').html(htmlStr);
+
+                        mui('.mui-scroll-wrapper').pullRefresh().endPulldownToRefresh();
+                        mui('.mui-scroll-wrapper').pullRefresh().refresh(true);
+                    });
+                }
+            },
+            up: {
+                callback: function () {   //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+                    currentPage++;
+                    render(function (info) {
+                        var htmlStr = template('productTpl', info);
+                        $('.lt_product').append(htmlStr);
+                        if (info.data.length === 0) {
+                            mui('.mui-scroll-wrapper').pullRefresh().endPullupToRefresh(true);
+                        } else {
+                            mui('.mui-scroll-wrapper').pullRefresh().endPullupToRefresh(false);
+                        }
+
+                    });
+                }
+            }
+        }
+    });
+
+    $('.lt_product').on('tap', 'a', function () {
+        location.href = $(this).attr('href');
     })
 
 })
