36e976cb3fd896638d1fbde36e2390831b8f5851
WenYuanZhi <544268402@qq.com>
2019-08-31 15:18:24 +0800

    登录页面完成

---
+++ public/front/js/login.js
@@ -0,0 +1,36 @@
+$(function () {
+    $('#loginBtn').click(function () {
+        var username = $('#username').val().trim();
+        var password = $('#password').val().trim();
+        if (username === "") {
+            mui.toast('请输入用户名');
+            return;
+        }
+        if (password === "") {
+            mui.toast('请输入密码');
+            return;
+        }
+        $.ajax({
+            type: 'post',
+            url: '/user/login',
+            data: { username: username, password: password },
+            dataType: 'json',
+            success: function (info) {
+
+                if (info.error === 403) {
+                    mui.toast('用户名或者密码不正确');
+                    return;
+                }
+
+                if (info.success) {
+                    if (location.search.indexOf('retUrl') > -1) {
+                        var retUrl = location.search.replace('?retUrl=', '');
+                        location.href = retUrl;
+                    } else {
+                        location.href = 'user.html';
+                    }
+                }
+            }
+        })
+    })
+})
