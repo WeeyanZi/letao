ccf79bd6ab75cfc32193a0374ff326588ed315b2
WenYuanZhi <544268402@qq.com>
2019-08-31 14:12:13 +0800

    添加购物车功能

--- public/front/js/product.js
+++ public/front/js/product.js
@@ -18,4 +18,37 @@ $(function () {
             mui('.mui-numbox').numbox();
         }
     })
+
+    $('.lt_main').on('click', '.lt_size span', function () {
+        $(this).addClass('current').siblings().removeClass('current');;
+    })
+
+    $('#addCart').click(function () {
+        var size = $('.lt_size span.current').text();
+        var num = $('.mui-numbox-input').val();
+        if (!size) {
+            mui.toast('请选择尺码')
+            return;
+        }
+
+        $.ajax({
+            type: 'post',
+            url: '/cart/addCart',
+            data: { productId: productId, num: num, size: size },
+            dataType: 'json',
+            success: function (info) {
+                
+                if (info.error === 400) {
+                    location.href = 'login.html?retUrl=' + location.href;
+                }
+                if (info.success) {
+                    mui.confirm('添加成功', '温馨提示', ['去购物车', '继续浏览'], function (e) {
+                        if (e.index === 0) {
+                            location.href = 'cart.html';
+                        }
+                    })
+                }
+            }
+        })
+    })
 })
